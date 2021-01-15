6fcc0bb75aafe1ccfeff0f6e1207584603556131
Kata Katona <katalap@gmail.com>
2021-01-15 00:23:40 +0000

    card view and adding details set up

---
+++ src/AddCardForm.js
@@ -0,0 +1,130 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './AddCardForm.css';
+
+const AddCardForm = ({ setName, handleNumberChange, setCardType, handleMonthChange, handleYearChange, setCVV }) => {
+
+    const [newName, setNewName] = useState('John Doe');
+    const [newCardNumber, setNewCardNumber] = useState(1234123412341234);
+    const [isInvalid, setIsInvalid] = useState(false);
+    const [isInvalidCVV, setIsInvalidCVV] = useState(false);
+    const [newCardType, setNewCardType] = useState('Visa');
+    const [newMonth, setNewMonth] = useState('');
+    const [newYear, setNewYear] = useState('');
+    const [newCVV, setNewCVV] = useState(0);
+
+    useEffect(() => {
+        const copyOfCardNo = newCardNumber;
+        const testIfNumber = Number(copyOfCardNo);
+        if (newCardNumber.length > 16 || Number.isNaN(testIfNumber)) {
+            setIsInvalid(true);
+        } else {
+            setIsInvalid(false);
+        }
+    }, [newCardNumber]);
+
+    useEffect(() => {
+        const copyOfCVV = newCVV;
+        const testIfNumber = Number(copyOfCVV);
+        if (newCVV.length > 3 || Number.isNaN(testIfNumber)) {
+            setIsInvalidCVV(true);
+        } else {
+            setIsInvalidCVV(false);
+        }
+    }, [newCVV]);
+
+    const handleSave = (e) => {
+        e.preventDefault();
+        console.log('save');
+    }
+
+    const handleNewNameChange = (e) => {
+        setName(e.target.value);
+        setNewName(e.target.value);
+    }
+
+    const handleNewNumberChange = (e) => {
+        setNewCardNumber(e.target.value);
+        handleNumberChange(e.target.value);
+    }
+
+    const handleCardTypeChange = (e) => {
+        setCardType(e.target.value);
+        setNewCardType(e.target.value);
+    }
+
+    const handleNewMonthChange = (e) => {
+        setNewMonth(e.target.value);
+        handleMonthChange(e.target.value);
+    }
+
+    const handleNewYearChange = (e) => {
+        handleYearChange(e.target.value);
+        setNewYear(e.target.value);
+    }
+
+    const handleNewCVVChange = (e) => {
+        setCVV(e.target.value);
+        setNewCVV(e.target.value);
+    }
+
+    return (
+        <div className="add-cc">
+            <form className="add-cc__credit-card">
+                <div className="add-cc__form-header">
+                    <h2 className="add-cc__title2">New Credit Card</h2>
+                </div>
+
+                <div className="add-cc__form-body">
+                    <input type="text" className="add-cc__name" placeholder="John Doe" onChange={handleNewNameChange}></input>
+                    <input type="text" className="add-cc__card-number" placeholder="Card Number" onChange={handleNewNumberChange}></input>
+                    {isInvalid ? <p className="invalid">Invalid! 16 digits required</p> : null}
+                    <div className="add-cc__date-field">
+                        <div className="add-cc__month">
+                            <select name="Month" onChange={handleNewMonthChange} >
+                                <option value="january">January</option>
+                                <option value="february">February</option>
+                                <option value="march">March</option>
+                                <option value="april">April</option>
+                                <option value="may">May</option>
+                                <option value="june">June</option>
+                                <option value="july">July</option>
+                                <option value="august">August</option>
+                                <option value="september">September</option>
+                                <option value="october">October</option>
+                                <option value="november">November</option>
+                                <option value="december">December</option>
+                            </select>
+                        </div>
+                        <div className="add-cc__year">
+                            <select name="Year" onChange={handleNewYearChange}>
+                                <option value="2021">2021</option>
+                                <option value="2022">2022</option>
+                                <option value="2023">2023</option>
+                                <option value="2024">2024</option>
+                                <option value="2024">2025</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className="add-cc__verification">
+                        <div className="add-cc__cvv">
+                            <input type="text" placeholder="CVV" onChange={handleNewCVVChange}></input>
+                            {isInvalidCVV ? <p className="add-cc__cvv-details alert">3 digits at the back</p> : <p className="add-cc__cvv-details">3 digits at the back</p> }
+                        </div>
+                        <div className="add-cc__type">
+                            <select name="Type" onChange={handleCardTypeChange}>
+                                <option value="visa">Visa</option>
+                                <option value="mastercard">MasterCard</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <button onClick={handleSave} className="add-cc__save-card">Save</button>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default AddCardForm;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import CardView from './CardView';
+import AddCardForm from './AddCardForm';
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState('John Doe');
+  const [cardNumber, setCardNumber] = useState('1234123412341234');
+  const [cardType, setCardType] = useState('Visa');
+  const [month, setMonth] = useState('10');
+  const [year, setYear] = useState('22');
+  const [CVV, setCVV] = useState('123');
+
+  const months = {
+    "january": 1,
+    "february": 2,
+    "march": 3,
+    "april": 4,
+    "may": 5,
+    "june": 6,
+    "july": 7,
+    "august": 8,
+    "september": 9,
+    "october": 10,
+    "november": 11,
+    "december": 12,
+  }
+
+  const years = {
+    "2021": 21,
+    "2022": 22,
+    "2023": 23,
+    "2024": 24,
+    "2025": 25,
+  }
+
+  const handleNumberChange = (number) => {
+    setCardNumber(number);
+  }
+
+  const handleMonthChange = (monthName) => {
+    const newMonth = months[monthName];
+    setMonth(newMonth);
+  }
+
+  const handleYearChange = (yearName) => {
+    const newYear = years[yearName];
+    setYear(newYear);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="homepage__title">Add New Card</h1>
       </header>
+      <section className="card">
+      <CardView  name={name} cardNumber={cardNumber} cardType={cardType} month={month} year={year} CVV={CVV} />
+      <AddCardForm setName={setName} handleNumberChange={handleNumberChange} setCardType={setCardType} handleMonthChange={handleMonthChange} handleYearChange={handleYearChange} setCVV={setCVV} />
+      </section>
     </div>
   );
 }
